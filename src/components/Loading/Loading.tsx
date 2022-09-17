e722b279ef8c91807ebcd6c304985cb029e45fbf
John Fernandes <john1625b@gmail.com>
2022-09-17 16:57:55 -0400

    fix redirect to (forgot to add /story to the navigate)

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -1,27 +1,26 @@
-import React, { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import React, {useEffect} from "react";
+import {useNavigate} from "react-router-dom";
 
 
 const Loading = () => {
 
-  // this will be api call
-  const getStory = () : string => {
-    return "urmom"
-  }
+    // this will be api call
+    const getStoryId = (): string => {
+        return "urmom"
+    }
 
-  const navigate = useNavigate();
-  useEffect(() => {
-    setTimeout(() => {
-      navigate(`/${getStory()}`);
-    }, 3000)
-  }, [])
+    const navigate = useNavigate();
+    useEffect(() => {
+        setTimeout(() => {
+            navigate(`story/${getStoryId()}`);
+        }, 3000)
+    }, [])
 
-  
-  return (
-    <div>
-      loading...
-    </div>
-  )
+    return (
+        <div>
+            loading...
+        </div>
+    )
 }
 
 export default Loading;
