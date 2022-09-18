cd52948b1e34ce164aba188d7163a9b8746a1f34
Ammar Ahmed <ammar.ahmed2203@gmail.com>
2022-09-18 00:00:29 -0400

    merge conflicts

--- src/components/Routing/Router.tsx
+++ src/components/Routing/Router.tsx
@@ -3,18 +3,6 @@ import StoryBuilder from "../StoryBuilder/StoryBuilder";
 import Authenticate from "../Auth/Authenticate";
 
 const Router = () => {
-<<<<<<< HEAD
-    const router = createBrowserRouter([
-        {
-            path: "/story/",
-            element: <StoryBuilder/>,
-        },
-        {
-            path: "/story/:storyId",
-            element: <StoryBuilder/>
-        }
-    ]);
-=======
   const router = createBrowserRouter([
     {
       path: "/",
@@ -22,11 +10,14 @@ const Router = () => {
       
     },
     {
+      path: "/story/",
+      element: <StoryBuilder/>,
+    },
+    {
       path: "/story/:storyId",
       element: <StoryBuilder />
     }
   ]);
->>>>>>> f9e2db9 (something weird going on idk styl)
 
 
     return <RouterProvider router={router}/>
